b985b3ba403106babc45bbe2119fb6ebb51c1ec5
Alison Rothmund <128057806+alisoRoth@users.noreply.github.com>
2024-05-06 08:23:13 -0300

    Add files via upload

--- Metas de vida/main.js
+++ Metas de vida/main.js
@@ -1,5 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo3 = new Date("2025-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
+const tempoAtual = new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
@@ -15,3 +21,19 @@ for(let i=0;i <botoes.length;i++){
         //Adiciona a classe ativo só no botão clicado.
     }
 }
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function calculaTempo (tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor (tempoFinal/1000);
+    let minutos = Math.floor (segundos/60);
+    let horas = Math.floor (minutos/60);
+    let dias = Math.floor (horas/24);
+    let mes = Math.floor (dias/30);
+    segundos %= 60;
+    minutos %=60;
+    horas %=24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    
+ }
